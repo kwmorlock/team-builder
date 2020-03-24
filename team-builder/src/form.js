2df2d87a4e2ab5b768ca2d6acd532fb9b0603dc3
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-24 16:52:52 -0400

    wrapped divs in labels

--- team-builder/src/form.js
+++ team-builder/src/form.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 <div>
-    
+    <form>
+        <label>
+<div>First Name:{}</div>
+        </label>
+
+        <label>
+<div>Last Name:{}</div>
+        </label>
+
+        <label>
+<div>Role:{}</div>
+        </label>
+        
+   </form>
 </div>
