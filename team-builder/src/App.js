f6bd27bc3ee1813604bfecb6a0ab0f667488dae9
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-24 15:52:00 -0400

    teamMember.js added

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,30 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const teamMembers = [
+  // const teamMembers = [
+  //   {firstName: 'kenzie', lastName: 'morlock', role: 'Goddess'},
+  //   {firstName: 'marissa', lastName: 'cooter', role: 'NonFurry'},
+  //   {firstName: 'zoe', lastName: 'stokes', role: 'Furry'},
+  //   {firstName: 'christina', lastName: 'harris', role: 'Cat Lady'},    
+  // ]
 
+  const [teamMembers, setTeamMembers] = useState([
     {firstName: 'kenzie', lastName: 'morlock', role: 'Goddess'},
     {firstName: 'marissa', lastName: 'cooter', role: 'NonFurry'},
     {firstName: 'zoe', lastName: 'stokes', role: 'Furry'},
     {firstName: 'christina', lastName: 'harris', role: 'Cat Lady'},    
-  ]
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+
   );
 }
 
